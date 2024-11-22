fd3a978a3167e05781627c90a56e8713100f28a6
Everton Luis de Siqueira <everton.siqueira@unimedvarginha.coop.br>
2024-11-22 16:40:59 -0300

    criação de rotas

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import app from "./src/app.js"
-
-const PORT = 3000;app.listen(PORT, () => {
-    console.log("servidor escutando!");
-});
+import app from"./src/app.js"
+const port = 3000
+app.listen(port, () =>{
+    console.log("Servidor escutando")
+})
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,57 @@
-import express from 'express';
+import express from "express"
 
-const app = express();
+const app = express ();
+app.use(express.json())
 
+
+const opcao = [
+    {
+        id:1,
+        opcao:"Omelete",
+        descricao:"blalsllsalslallslall"
+    },
+    {
+        id:2,
+        opcao:"Omelete",
+        descricao:"blalsllsalslallslall"
+    },
+    {
+        id:3,
+        opcao:"Omelete",
+        descricao:"blalsllsalslallslall"
+    }
+    
+]
+
+function buscaOpcao(id){
+    return opcao.findIndex(opcoes =>{
+        return opcoes.id === Number(id)
+    });
+};
+// testando a rota base
 app.get("/", (req, res) => {
-  res.status(200).send("Curso de Node.js");
+    res.status(200).send("Api Funcionando");
 });
+// pegando informações que estão no array opcao
+app.get("/opcao", (req,res) =>{
+    res.status(200).json(opcao);
+
+// Inserindo informações no array.
+});
+app.post("/opcao", (req,res) =>{
+opcao.push(req.body);
+res.status(201).send("Opção inserida com sucesso");
+
+});
+//buscando opcao por id
+app.get("/opcao/:id", (req,res) =>{
+    const index = buscaOpcao(req.params.id);
+    res.status(201).json(opcao[index]);
+});
+app.put("/opcao/:id",(req,res) =>{
+    const index = buscaOpcao(req.params.id);
+    opcao[index].descricao = req.body.descricao;//
+    res.status(200).json(opcao)
+})
 
 export default app;
